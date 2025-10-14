8793fe9b4d9b555186312401cea7577f4ce8e4d3
takano takuya <2311140058h@ed.fuk.kindai.ac.hp>
2025-10-14 07:15:35 +0000

    Fix Rubocop lint issues (quotes and whitespace)

--- test/controllers/cartoons_controller_test.rb
+++ test/controllers/cartoons_controller_test.rb
@@ -1,35 +1,33 @@
 require "test_helper"
 
 class CartoonsControllerTest < ActionDispatch::IntegrationTest
-  
   setup do
     @cartoon = cartoons(:cartoon_one)
   end
 
-  test 'cartoonを１つ表示すること' do
+  test "cartoonを１つ表示すること" do
     get "/cartoons/#{@cartoon.id}"
     assert_response :success
   end
 
-  test 'newテンプレートをrenderすること' do
+  test "newテンプレートをrenderすること" do
     get "/cartoons/new"
     assert_response :success
   end
 
-  test 'cartoonを作成できること' do
-    assert_difference('Cartoon.count') do
+  test "cartoonを作成できること" do
+    assert_difference("Cartoon.count") do
       post "/cartoons", params: { cartoon: { title: "進撃の巨人", author: "諫山創", description: "巨人との戦いの物語" } }
     end
     assert_equal "１レコード追加しました", flash[:notice]
     assert_redirected_to cartoon_path(Cartoon.last)
   end
 
-  test '無効なパラメータではcartoonを作成できないこと' do
-    assert_no_difference('Cartoon.count') do
+  test "無効なパラメータではcartoonを作成できないこと" do
+    assert_no_difference("Cartoon.count") do
       post "/cartoons", params: { cartoon: { title: "", author: "", description: "" } }
     end
     assert_equal "追加に失敗しました", flash[:alert]
-    # Rails 8では422を返す仕様
     assert_response :unprocessable_content
   end
 
@@ -50,9 +48,8 @@ class CartoonsControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_content
   end
 
-  
   test "cartoonを削除できること" do
-    assert_difference('Cartoon.count', -1) do
+    assert_difference("Cartoon.count", -1) do
       delete "/cartoons/#{@cartoon.id}"
     end
     assert_equal "１レコード削除しました", flash[:notice]
